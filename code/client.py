ffe9bae51f8d70eb1b6140ffc2784299335729df
Anand Umashankar <anandcu3@gmail.com>
2019-10-06 16:49:20 +0300

    Added multithreaded performance tests for concurrent requests

--- code/client.py
+++ code/client.py
@@ -2,21 +2,42 @@ import pandas as pd
 import requests
 import time
 import threading
+import numpy as np
+
+
+class DataPushThread(threading.Thread):
+     def __init__(self, number_of_requests=10):
+         super(DataPushThread, self).__init__()
+         self.number_of_requests = number_of_requests
+
+     def run(self):
+        prev_time = time.time()
+        response_times = []
+        for i in range(self.number_of_requests):
+            single_row = playstoreData.sample()
+            print("Sending data for app : ", ascii(single_row.loc[single_row.index[0],'App']))
+            row_kv_format = single_row.to_dict(orient='records')
+            row_kv_format[0]["id"] = int(single_row.index[0])
+            start_time = time.time()
+            r = requests.post(server_address + "ingestdata", json=row_kv_format)
+            time_taken = time.time() - start_time
+            response_times.append(time_taken)
+            print("Received code : ",r.status_code, r.reason, "in ", time_taken,"seconds." )
+            if r.status_code != 200:
+                print("############################")
+                print("REQUEST FAILED!!!", int(single_row.index[0]))
+                print("############################")
+        print("asdfghjkl",time.time()-prev_time)
+        print(np.mean(response_times),np.max(response_times),np.min(response_times), np.std(response_times))
+
+
 
 server_address = "http://127.0.0.1:5000/"
 playstoreData = pd.read_csv('../data/googleplaystore.csv')
-number_of_requests = 10000
-
-for i in range(number_of_requests):
-    single_row = playstoreData.sample()
-    print("Sending data for app : ", ascii(single_row.loc[single_row.index[0],'App']))
-    row_kv_format = single_row.to_dict(orient='records')
-    row_kv_format[0]["id"] = int(single_row.index[0])
-    start_time = time.time()
-    r = requests.post(server_address + "ingestdata", json=row_kv_format)
-    time_taken = time.time() - start_time
-    print("Received code : ",r.status_code, r.reason, "in ", time_taken,"seconds." )
-    if r.status_code != 200:
-        print("############################")
-        print("REQUEST FAILED!!!", int(single_row.index[0]))
-        print("############################")
+number_of_concurrent_threads = 1
+#print("Starting ",number_of_concurrent_threads, "threads")
+prev_time = 0
+for _ in range(number_of_concurrent_threads):
+    thr = DataPushThread()
+    thr.start()
+    prev_time=time.time()
--- code/rest_api.py
+++ code/rest_api.py
@@ -1,14 +1,15 @@
 from flask import Flask, request
 import pymongo
+import urllib
 
-client = pymongo.MongoClient('localhost', 27017)
+client = pymongo.MongoClient("mongodb://anandcu3:"+urllib.parse.quote("Rko@61999")+"@cluster0-shard-00-00-fbaws.gcp.mongodb.net:27017,cluster0-shard-00-01-fbaws.gcp.mongodb.net:27017,cluster0-shard-00-02-fbaws.gcp.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client["google_play_store"]
 apps = db["app_information"]
 app = Flask(__name__)
 
 
 @app.route('/ingestdata', methods=['POST'])
-def hello():
+def ingestData():
     x = apps.insert(request.json)
     print(x)
     return ""
