e4e8df5a90871b63b2b78f6acb454f1a0e76bcb8
Anand Umashankar <anandcu3@gmail.com>
2019-10-06 13:17:32 +0300

    Added performance tests to check large number of requests

--- code/client.py
+++ code/client.py
@@ -1,8 +1,22 @@
 import pandas as pd
 import requests
+import time
+import threading
+
 server_address = "http://127.0.0.1:5000/"
 playstoreData = pd.read_csv('../data/googleplaystore.csv')
-single_row = playstoreData.sample()
-row_kv_format = single_row.to_dict(orient='records')
-r = requests.post(server_address + "ingestdata", json=row_kv_format)
-print(r.status_code, r.reason)
+number_of_requests = 10000
+
+for i in range(number_of_requests):
+    single_row = playstoreData.sample()
+    print("Sending data for app : ", ascii(single_row.loc[single_row.index[0],'App']))
+    row_kv_format = single_row.to_dict(orient='records')
+    row_kv_format[0]["id"] = int(single_row.index[0])
+    start_time = time.time()
+    r = requests.post(server_address + "ingestdata", json=row_kv_format)
+    time_taken = time.time() - start_time
+    print("Received code : ",r.status_code, r.reason, "in ", time_taken,"seconds." )
+    if r.status_code != 200:
+        print("############################")
+        print("REQUEST FAILED!!!", int(single_row.index[0]))
+        print("############################")
--- code/rest_api.py
+++ code/rest_api.py
@@ -1,9 +1,14 @@
-from flask import Flask, escape, request
+from flask import Flask, request
+import pymongo
 
+client = pymongo.MongoClient('localhost', 27017)
+db = client["google_play_store"]
+apps = db["app_information"]
 app = Flask(__name__)
 
 
 @app.route('/ingestdata', methods=['POST'])
 def hello():
-    print(request.json)
+    x = apps.insert(request.json)
+    print(x)
     return ""
