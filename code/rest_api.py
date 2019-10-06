33e5399e37db19d21aa96827d0c99c30813febcb
Anand Umashankar <anandcu3@gmail.com>
2019-10-06 18:26:36 +0300

    Made code changes to move to GCP from local machine

--- code/rest_api.py
+++ code/rest_api.py
@@ -1,9 +1,8 @@
 from flask import Flask, request
 import pymongo
 import urllib
-import password_mongo
 
-client = pymongo.MongoClient("mongodb://anandcu3:"+urllib.parse.quote(password_mongo.pass)+"@cluster0-shard-00-00-fbaws.gcp.mongodb.net:27017,cluster0-shard-00-01-fbaws.gcp.mongodb.net:27017,cluster0-shard-00-02-fbaws.gcp.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb://anandcu3:h3GuvlswXSUsRgCb@cluster0-shard-00-00-fbaws.gcp.mongodb.net:27017,cluster0-shard-00-01-fbaws.gcp.mongodb.net:27017,cluster0-shard-00-02-fbaws.gcp.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client["google_play_store"]
 apps = db["app_information"]
 app = Flask(__name__)
