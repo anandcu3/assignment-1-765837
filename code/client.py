f6dd377e19030960ec20891cde6117b0f23b5d9f
Anand Umashankar <anandcu3@gmail.com>
2019-10-08 00:37:01 +0300

    Added REST APIs to search for app information and for app reviews

--- code/client.py
+++ code/client.py
@@ -3,10 +3,44 @@ import requests
 import time
 import threading
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--number_of_concurrent_threads", help="Number of concurrent threads to push to the DAAS platform. Default is 6.",
+                    type=int, default=6 )
+parser.add_argument("-r", "--total_number_requests", help="Total number of data points to be pushed via the daas. Default is 10000. Will be dividied among all threads",
+                    type=int, default=10000 )
+parser.add_argument("--all", help="Use this argument to just push all the rows of the csv file through DAAS without random sampling.",
+                    action="store_true", default=False )
+parser.add_argument("--reviews", help="Use this argument to push data from the reviews file instead of the zpp information file.",
+                    action="store_true", default=False)
+args = parser.parse_args()
+
+push_the_CSV = args.all
+reviewData = args.reviews
+number_of_concurrent_threads = args.number_of_concurrent_threads
+number_of_requests = args.total_number_requests//number_of_concurrent_threads
+
+server_address = "http://127.0.0.1:5000/"
+
+def sendRequest(row_kv_format, server_address):
+    start_time = time.time()
+    if reviewData:
+        server_address = server_address + "appreviews/"
+    else:
+        server_address = server_address + "appinfo/"
+    r = requests.post(server_address + "ingestdata", json=row_kv_format)
+    time_taken = time.time() - start_time
+    print("Received code : ",r.status_code, r.reason, "in ", time_taken,"seconds." )
+    if r.status_code != 200:
+        print("############################")
+        print("REQUEST FAILED!!!")
+        print("############################")
+    return time_taken
 
 
 class DataPushThread(threading.Thread):
-     def __init__(self, number_of_requests=5000):
+     def __init__(self, number_of_requests):
          super(DataPushThread, self).__init__()
          self.number_of_requests = number_of_requests
 
@@ -18,26 +52,24 @@ class DataPushThread(threading.Thread):
             print("Sending data for app : ", ascii(single_row.loc[single_row.index[0],'App']))
             row_kv_format = single_row.to_dict(orient='records')
             row_kv_format[0]["id"] = int(single_row.index[0])
-            start_time = time.time()
-            r = requests.post(server_address + "ingestdata", json=row_kv_format)
-            time_taken = time.time() - start_time
+            time_taken = sendRequest(row_kv_format, server_address)
             response_times.append(time_taken)
-            print("Received code : ",r.status_code, r.reason, "in ", time_taken,"seconds." )
-            if r.status_code != 200:
-                print("############################")
-                print("REQUEST FAILED!!!", int(single_row.index[0]))
-                print("############################")
-        print("asdfghjkl",time.time()-prev_time)
+        print("Time taken for execution of the thread: ",time.time()-prev_time)
         print(np.mean(response_times),np.max(response_times),np.min(response_times), np.std(response_times))
 
 
+if reviewData:
+    playstoreData = pd.read_csv('../data/googleplaystore_user_reviews.csv')
+else:
+    playstoreData = pd.read_csv('../data/googleplaystore.csv')
 
-server_address = "http://35.228.81.87/"
-playstoreData = pd.read_csv('../data/googleplaystore.csv')
-number_of_concurrent_threads = 2
-#print("Starting ",number_of_concurrent_threads, "threads")
-prev_time = 0
-for _ in range(number_of_concurrent_threads):
-    thr = DataPushThread()
-    thr.start()
-    prev_time=time.time()
+if push_the_CSV:
+    playstoreData['Id'] = playstoreData.index
+    for i in playstoreData.index:
+        single_row = playstoreData.iloc[[i]]
+        row_to_ingest_json = single_row.to_dict(orient='records')
+        time_taken = sendRequest(row_to_ingest_json, server_address)
+else:
+    for _ in range(number_of_concurrent_threads):
+        thr = DataPushThread(number_of_requests)
+        thr.start()
--- code/mysimbdp-daas.py
+++ code/mysimbdp-daas.py
@@ -1,17 +1,49 @@
 from flask import Flask, request
+from flask_restplus import Resource, Api
 import pymongo
-import urllib
+import jsons
 
 client = pymongo.MongoClient("mongodb://anandcu3:h3GuvlswXSUsRgCb@cluster0-shard-00-00-fbaws.gcp.mongodb.net:27017,cluster0-shard-00-01-fbaws.gcp.mongodb.net:27017,cluster0-shard-00-02-fbaws.gcp.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client["google_play_store"]
-apps = db["app_information"]
-app = Flask(__name__)
+apps_table = db["app_information"]
+reviews_table = db["app_reviews"]
+flaskApp = Flask(__name__)
+app = Api(app = flaskApp)
+app_information = app.namespace('appinfo', description='APIs concerning information about the applications from the play store')
+app_reviews = app.namespace('appreviews', description='APIs concerning the reviews about the applications')
 
+@app_information.route('/ingestdata')
+class ingestData(Resource):
+    def post(self):
+        x = apps_table.insert(request.json)
+        return ""
 
-@app.route('/ingestdata', methods=['POST'])
-def ingestData():
-    x = apps.insert(request.json)
-    print(x)
-    return ""
+@app_information.route('/name/<name>')
+class findAppbyName(Resource):
+    def get(self, name):
+        apps_table.create_index([('App', 'text')])
+        response = list(apps_table.find({"$text": {"$search": name}}))
+        return jsons.dumps(response)
+    def post(self, name):
+        apps_table.create_index([('App', 'text')])
+        response = list(apps_table.find({"$text": {"$search": name}}))
+        return jsons.dumps(response)
 
-app.run(host='0.0.0.0', port=80)
+@app_reviews.route('/ingestdata')
+class ingestData(Resource):
+    def post(self):
+        x = reviews_table.insert(request.json)
+        return ""
+
+@app_reviews.route('/name/<name>')
+class findAppbyName(Resource):
+    def get(self, name):
+        apps_table.create_index([('App', 'text')])
+        response = list(apps_table.find({"$text": {"$search": name}}))
+        return jsons.dumps(response)
+    def post(self, name):
+        apps_table.create_index([('App', 'text')])
+        response = list(apps_table.find({"$text": {"$search": name}}))
+        return jsons.dumps(response)
+
+flaskApp.run(host="0.0.0.0", port=80)
